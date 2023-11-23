ebfe919e67e455879e36045164f3da0f00d52fe5
determinedguy <mhd.athallah@gmail.com>
2023-11-23 13:39:20 +0700

    remove: sponsorship

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
     <div class="fixed z-50 top-[4.5rem] left-4" id="vue">
         <magic-bar></magic-bar>
     </div>
-    <!-- <livewire:sponsorship /> -->
     <main class="pb-10 main max-w-screen-2xl">
         {{ $slot }}
     </main>
